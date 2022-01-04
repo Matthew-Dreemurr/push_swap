f294a8ef7f6e2bbc5fe50b9b281bbe1766cace99
Matthew-Dreemurr <hadadmat@gmail.com>
2022-01-04 17:05:09 +0100

    Makefile push 2022-01-04 17:05:09

--- src/0_parser/ps_arg_init.c
+++ src/0_parser/ps_arg_init.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:07:07 by mahadad           #+#    #+#             */
-/*   Updated: 2022/01/04 16:00:56 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/01/04 17:00:20 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 void	ps_arg_init(int ac, const char **av, t_data *data)
 {
-	data->size = ac - 1;
+	data->size = ac;
 	data->set = malloc(sizeof(int) * data->size);
 	if (!data->set)
 		ps_exit_prog(EXIT_FAILURE, data, "Fail malloc data->set\n");
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 16:16:55 by mahadad           #+#    #+#             */
-/*   Updated: 2022/01/04 16:00:44 by mahadad          ###   ########.fr       */
+/*   Updated: 2022/01/04 17:01:16 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 #include "ps_parser.h"
 #include "ps_exit_prog.h"
 #include <stdlib.h>
+#include "libft.h"
+
+static void	arg_compactor(char **arg, int size)
+{
+	
+}
 
 int	main(int ac, const char **av)
 {
 	t_data	data;
 
-	if (ac < 2)
+	if (ac > 2)
+		arg_compactor(av + 1, ac - 1);
+	else if (!(ac == 2))
 		ps_exit_prog(EXIT_FAILURE, NULL, "Bad ac amount\n");
-	av++;
-	ps_arg_init(ac, av, &data);
+	ps_arg_init(ac - 1, av + 1, &data);
 	return (0);
 }
